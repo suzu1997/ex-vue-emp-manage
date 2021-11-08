a2456cdad3192834c95da6378394e039f012c801
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-08 16:03:01 +0900

    ログイン画面へのリンクを設定

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="header">
         <div class="header-left">
-          <router-link to="/registerAdmin">
+          <router-link to="/loginAdmin">
             <img class="logo" src="img/header_logo.png"
           /></router-link>
         </div>
@@ -12,9 +12,9 @@
           <router-link to="/registerAdmin">
             管理者登録
           </router-link>
-          <a href="../../documents/html_mock/login.html">
+          <router-link to="/loginAdmin">
             <i class="fas fa-sign-in-alt"></i>ログイン
-          </a>
+          </router-link>
           <a href="../../documents/html_mock/login.html">
             <i class="fas fa-sign-in-alt"></i>ログアウト
           </a>
