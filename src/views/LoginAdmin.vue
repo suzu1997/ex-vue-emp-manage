d3a4af4f6953d9dbec228483a9ded8c033106334
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-08 15:17:40 +0900

    LoginAdmin.vueのtemplete内で画面を作成

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -1,13 +1,59 @@
 <template>
-  
+  <div class="container">
+    <div class="row login-page">
+      <div class="col s12 z-depth-6 card-panel">
+        <div class="error">{{ errorMessage }}</div>
+        <form class="login-form">
+          <div class="row"></div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">mail_outline</i>
+              <input
+                class="validate"
+                id="mailAddress"
+                type="email"
+                v-model="mailAddress"
+              />
+              <label for="mailAddress" data-error="wrong" data-success="right"
+                >メールアドレス</label
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">lock_outline</i>
+              <input id="password" type="password" v-model="password" />
+              <label for="password">パスワード</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <button
+                class="btn btn-register waves-effect waves-light col s12"
+                v-on:click="loginAdmin"
+                type="button"
+              >
+                ログイン
+              </button>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6 m6 l6">
+              <p class="margin medium-small">
+                <router-link to="/registerAdmin"
+                  >管理者登録はこちら</router-link
+                >
+              </p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
