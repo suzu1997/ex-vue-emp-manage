eb41bf3d3d12999ed89ac24be7d726bb49210fd8
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-08 12:01:54 +0900

    Vuex ストアファイルの作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,94 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import { Employee } from "../types/employee";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
-  state: {}, // end state
-  actions: {}, // end actions
-  mutations: {}, // end mutations
-  getters: {}, // end getters
+  state: {
+    // 従業員数
+    totalEmployeeCount: 0,
+    // 従業員一覧
+    employees: new Array<Employee>(),
+  }, // end state
+  actions: {
+    /**
+     * 従業員一覧情報を取得.
+     *
+     * @remarks
+     * 従業員一覧情報を WebAPI から取得 して mutation を呼び出す
+     *
+     * @param context - コンテキストオブジェクト
+     */
+    async getEmployeeList(context) {
+      const response = await axios.get(
+        "http://153.127.48.168:8080/ex-emp-api/employee/employees"
+      );
+      const payload = response.data;
+      context.commit("showEmployeeList", payload);
+    },
+  }, // end actions
+  mutations: {
+    /**
+     * 従業員一覧情報を作成してstateに格納.
+     *
+     * @param state - stateオブジェクト
+     * @param payload - WebAPI から取得した JSON
+     */
+    showEmployeeList(state, payload) {
+      state.totalEmployeeCount = payload.totalEmployeeCount;
+      for (const employee of payload.employees) {
+        state.employees.push(
+          new Employee(
+            employee.id,
+            employee.name,
+            employee.image,
+            employee.gender,
+            employee.hireDate,
+            employee.mailAddress,
+            employee.zipCode,
+            employee.address,
+            employee.telephone,
+            employee.salary,
+            employee.characteristics,
+            employee.dependentsCount
+          )
+        );
+      }
+    },
+  }, // end mutations
+  getters: {
+    /**
+     * 従業員数を返す.
+     *
+     * @param state - stateオブジェクト
+     * @return 従業員数
+     */
+    getEmployeeCount(state) {
+      return state.totalEmployeeCount;
+    },
+    /**
+     * 従業員一覧を返す.
+     *
+     * @param state - stateオブジェクト
+     * @return 従業員一覧情報
+     */
+    getEmployees(state) {
+      return state.employees;
+    },
+    /**
+     * ID から従業員を検索し返す.
+     *
+     * @param state - stateオブジェクト
+     * @return 渡された ID で絞り込んだ Employee オブジェクト
+     */
+    getEmployeeById(state) {
+      return (id: number) => {
+        return state.employees.filter(employee => employee.id === id)[0];
+      };
+    },
+  }, // end getters
   modules: {}, // end modules
 });
