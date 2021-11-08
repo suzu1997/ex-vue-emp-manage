899672c3a741a6da4b10b38dcd050eabee08b770
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-08 16:33:21 +0900

    従業員一覧を表示する機能のクラスコンポーネントを作成

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -1,13 +1,34 @@
-<template>
-  
-</template>
+<template> </template>
 
-<script>
-export default {
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Employee } from "../types/employee";
 
+/**
+ * 従業員一覧表示機能のクラスコンポーネントです.
+ */
+@Component
+export default class EmployeeList extends Vue {
+  /**
+   * Vuexストアのアクション経由で非同期でWebAPIから従業員一覧を取得.
+   */
+  created(): void {
+    this["$store"].dispatch("getEmployeeList");
+  }
+  /**
+   * 非同期で取得したVuexストア内の従業員数を取得し返す.
+   */
+  get getEmployeeCount(): number {
+    return this["$store"].getters.getEmployeeCount;
+  }
+
+  /**
+   * 非同期で取得したVuexストア内の従業員一覧を取得し返す.
+   */
+  get getEmployees(): Array<Employee> {
+    return this["$store"].getters.getEmployees;
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
