c9b8a2b475b23960405af8f9dfa9dd2c6e8155f6
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-08 14:46:53 +0900

    ヘッダーとフッターが重複していたため、vueファイルないから削除

--- src/views/RegisterAdmin.vue
+++ src/views/RegisterAdmin.vue
@@ -1,91 +1,68 @@
 <template>
-  <div>
-    <!-- ヘッダー -->
-    <div class="container">
-      <div class="header">
-        <div class="header-left">
-          <a href="login.html">
-            <img class="logo" src="img/header_logo.png" />
-          </a>
+  <div class="container">
+    <div class="row register-page">
+      <div class="error">{{ errorMessage }}</div>
+      <form class="col s12" id="reg-form">
+        <div class="row">
+          <div class="input-field col s6">
+            <input
+              id="last_name"
+              type="text"
+              class="validate"
+              required
+              v-model="lastName"
+            />
+            <label for="last_name">姓</label>
+          </div>
+          <div class="input-field col s6">
+            <input
+              id="first_name"
+              type="text"
+              class="validate"
+              required
+              v-model="firstName"
+            />
+            <label for="first_name">名</label>
+          </div>
         </div>
-
-        <div class="header-right">
-          <a href="registerAdmin.html">管理者登録</a>
+        <div class="row">
+          <div class="input-field col s12">
+            <input
+              id="email"
+              type="email"
+              class="validate"
+              required
+              v-model="mailAddress"
+            />
+            <label for="email">メールアドレス</label>
+          </div>
         </div>
-      </div>
-    </div>
-    <!-- top-wrapper -->
-    <div class="container">
-      <div class="row register-page">
-        <div class="error">{{ errorMessage }}</div>
-        <form class="col s12" id="reg-form">
-          <div class="row">
-            <div class="input-field col s6">
-              <input
-                id="last_name"
-                type="text"
-                class="validate"
-                required
-                v-model="lastName"
-              />
-              <label for="last_name">姓</label>
-            </div>
-            <div class="input-field col s6">
-              <input
-                id="first_name"
-                type="text"
-                class="validate"
-                required
-                v-model="firstName"
-              />
-              <label for="first_name">名</label>
-            </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input
+              id="password"
+              type="password"
+              class="validate"
+              minlength="8"
+              required
+              v-model="password"
+            />
+            <label for="password">パスワード</label>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <input
-                id="email"
-                type="email"
-                class="validate"
-                required
-                v-model="mailAddress"
-              />
-              <label for="email">メールアドレス</label>
-            </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6">
+            <button
+              class="btn btn-large btn-register waves-effect waves-light"
+              v-on:click="registerAdmin"
+              type="button"
+            >
+              登録
+              <i class="material-icons right">done</i>
+            </button>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <input
-                id="password"
-                type="password"
-                class="validate"
-                minlength="8"
-                required
-                v-model="password"
-              />
-              <label for="password">パスワード</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="input-field col s6">
-              <button
-                class="btn btn-large btn-register waves-effect waves-light"
-                v-on:click="registerAdmin"
-                type="button"
-              >
-                登録
-                <i class="material-icons right">done</i>
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-    <!-- フッター -->
-    <div class="container">
-      <div class="footer">
-        <div class="footer-center">©️ xxx Inc.</div>
-      </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
