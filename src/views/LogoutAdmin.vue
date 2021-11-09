e0474e3340d27c572e8eb904529d1de8cc6ad9bd
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-09 12:02:55 +0900

    ログアウトをする機能のクラスコンポーネントを作成

--- src/views/LogoutAdmin.vue
+++ src/views/LogoutAdmin.vue
@@ -1,9 +1,18 @@
-<script>
-export default {
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
 
+/**
+ * ログアウトをする機能のクラスコンポーネントです.
+ */
+@Component
+export default class LogoutAdmin extends Vue {
+  /**
+   * ログアウトをする.
+   */
+  async logoutAdmin(): Promise<void> {
+    await axios.get("http://153.127.48.168:8080/ex-emp-api/logout");
+    this["$router"].push("/loginAdmin");
+  }
 }
 </script>
-
-<style>
-
-</style>
